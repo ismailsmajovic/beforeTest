ab57b13fa9be45b8452b2d3ef78a6dc0d4764125
Ismail <ismajovic20@gmail.com>
2022-07-01 12:38:37 +0200

    excercise

--- promise/promise.js
+++ promise/promise.js
@@ -1,27 +1,40 @@
 const obecanje = new Promise((resolve, reject)=>{
     response = 200;
-
     setTimeout(()=>{
-
         for(let i = 0; i < 900000; i++) {
-            if (response === 9000) {
-                resolve[1, 2, 3, 4, 5 ];
-            }else{
+           
+            if (response === 200) {
+                resolve([1, 2, 3, 4, 5 ]);
+            }else if(response === 201){
                 reject('doslo je do neke greske:');
+            }else if(response < 200){
+                reject('nastavi sa disanjem')
             }
         }
-    }, 3000)
     
+    }, 4000)
+       
 });
 
 obecanje.then((mes)=>{
-    console.log(mes)
+    console.log('uspesno', mes)
 })
 .catch((mes)=>{
-    console.log(mes)
+    console.log('neupesno', mes)
+})
+.finally((mes)=>{
+    console.log('zivi', mes)
 })
 
-console.log('pocinjemo sa programom');
-setTimeout(()=>{
-    console.log('-----------------')
-}, 1000)
+
+
+console.log('pocinjemo sa programom...');
+for(let i = 0; i < 20; i++){
+    setTimeout(()=>{
+        broji()
+        broji()
+    }, 3000)
+}
+function broji(){
+    console.log('-----')
+}
